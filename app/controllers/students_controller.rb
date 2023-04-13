e8638ccec6a745e231237a286f332b638f87294d
Samuel Amity <sam.amity@gmail.com>
2023-04-13 18:20:19 -0400

    Lab completed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params.include?(:name)
+      name = params[:name].capitalize
+      students = Student.all.where('first_name = ? OR last_name = ?', name, name)
+    else
+      students = Student.all
+    end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
